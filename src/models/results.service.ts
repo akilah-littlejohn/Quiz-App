334eb2905b8357556cc2d15d5f5240ad28102dd9
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-05-11 22:52:45 -0500

    1 file modified

--- src/models/results.service.ts
+++ src/models/results.service.ts
@@ -5,5 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ResultsService {
 
+// set up logic for storing answers selected by users
+// tally number of answered correct or incorrect
 
 }
