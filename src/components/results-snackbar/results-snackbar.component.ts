a17b3a33c5b648a492bcd3b1e1b25d65c5aad04d
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-05-11 23:01:11 -0500

    2 files added

---
+++ src/components/results-snackbar/results-snackbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-snackbar',
+  templateUrl: './results-snackbar.component.html',
+  styleUrls: ['./results-snackbar.component.css']
+})
+export class ResultsSnackbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
