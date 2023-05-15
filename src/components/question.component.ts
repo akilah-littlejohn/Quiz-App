02fb1b538d1481f11c9e75c74fe562cf0279b672
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-05-14 23:25:03 -0500

    2 files modified

--- src/components/question.component.ts
+++ src/components/question.component.ts
@@ -6,19 +6,32 @@ import {
   Input,
   EventEmitter,
 } from '@angular/core';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { QuestionComponent } from './question.component';
+
 import { QuizService } from '../models/quiz.service';
 
 @Component({
   selector: 'app-question',
   template: 
   `    
-    <p> {{ question }} </p>
-    <label><input type="radio" name="" [value]=""></label>
+   
 `,
   styles: [],
   standalone: true,
-  imports: [],
-})
+  imports: [
+    CommonModule,
+    MatStepperModule,
+    MatIconModule,
+    MatRadioModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    QuestionComponent
+  ],
 export class QuestionComponent implements OnInit {
   quiz = inject<QuizService>(QuizService);
   @Input() question: string;
--- src/components/quiz.component.ts
+++ src/components/quiz.component.ts
@@ -1,11 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatIconModule } from '@angular/material/icon';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatButtonModule } from '@angular/material/button';
-import { QuestionComponent } from './question.component';
 
 @Component({
   selector: 'app-quiz',
@@ -18,14 +12,6 @@ import { QuestionComponent } from './question.component';
   `,
   styles: [],
   standalone: true,
-  imports: [
-    CommonModule,
-    MatStepperModule,
-    MatIconModule,
-    MatRadioModule,
-    MatSnackBarModule,
-    MatButtonModule,
-    QuestionComponent
-  ],
+  
 })
 export class QuizComponent {}
