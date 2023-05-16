514db63364febe276d1ada57b8628684baafd9d0
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-05-15 21:32:15 -0500

    1 file modified

--- src/models/results.service.ts
+++ src/models/results.service.ts
@@ -2,32 +2,27 @@ import { Injectable, inject } from '@angular/core';
 import { QuizService } from './quiz.service';
 
 @Injectable({
-  providedIn:'root'
+  providedIn: 'root',
 })
 export class ResultsService {
+  showResult = false;
+  score = 0;
+  quizService = inject<QuizService>(QuizService);
 
-showResult = false;
-score = 0;
-quizService = inject<QuizService>(QuizService);
-
-onSelectedAnswer(answer:string){
-  this.quizService.checkAnswer(answer)
-}
-getTriviaScore(){
-  let count = 0;
-  this.quizService.triviaQuestions.forEach((question, index) => {
-    if (this.quizService.userAnswers[index] === question.answer) {
-      count++;
-    }
-  });
-  return (count/this.quizService.triviaQuestions.length * 100)
-
+  onSelectedAnswer(answer: string) {
+    this.quizService.checkAnswer(answer);
+  }
+  getTriviaScore() {
+    let count = 0;
+    this.quizService.triviaQuestions.forEach((question, index) => {
+      if (this.quizService.userAnswers[index] === question.answer) {
+        count++;
+      }
+    });
+    return (count / this.quizService.triviaQuestions.length) * 100;
   }
 
-  submitTrivia(){
-    this.score = this.getTriviaScore()
+  submitTrivia() {
+    this.score = this.getTriviaScore();
   }
 }
-
-
-
