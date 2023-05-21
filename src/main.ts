60e876e50fd7ab30c128eab94bce0414c2f597db
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-05-21 10:39:57 -0500

    1 file modified, 1 deleted

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { QuizComponent } from './components/quiz.component';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-import { routes } from './route';
 
 
 
 bootstrapApplication(QuizComponent, {
-  providers:[importProvidersFrom(BrowserAnimationsModule),
-  provideRouter(routes)]
+  providers:[importProvidersFrom(BrowserAnimationsModule)]
+ 
 });
